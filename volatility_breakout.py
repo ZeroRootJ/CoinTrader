2d55a46ca75b326f0eca78d4963e076314e33569
ZeroRootJ <birdonmars2001@gmail.com>
2022-01-06 11:23:54 +0000

    ADD trader using volatility breakout

---
+++ volatility_breakout.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 변동성 돌파 전략 Volatility breakout
+# 
+# 가격변동폭 계산 : 전일 고가 - 전일 저가
+# 
+# 매수 기준 : 당일 시간에서 변동폭 * (k=0.5) 이상 상승시 해당 가격에 매수
+# 
+# 매도 기준 : 당일 종가에 매도
+
+# In[24]:
+
+
+from pyupbit import *
+import time
+import datetime
+
+
+# In[25]:
+
+
+def buy_crypto(ticker):
+    pass
+
+def sell_crypto(ticker):
+    pass
+
+
+# In[26]:
+
+
+def get_target_price(ticker,k):
+    pd = get_ohlcv(ticker)
+    price_diff = pd.iloc[-2]['high'] - pd.iloc[-2]['low']
+    target = pd.iloc[-1]['open'] + price_diff * k
+    return target
+
+
+# In[29]:
+
+
+ticker = "KRW-BTC"
+now = datetime.datetime.now()
+m_open = datetime.datetime(now.year,now.month,now.day) + datetime.timedelta(1.375)
+target = get_target_price(ticker,0.5)
+print("[START] Volatility Breakout TRADER")
+
+while True:
+    try:
+        # SELL ALL & update target price when market starts
+        now = datetime.datetime.now()
+        if m_open < now < m_open + datetime.timedelta(seconds=10):
+            sell_crypto(ticker)
+            print("[SELL] ", str(datetime.datetime.now()))
+            flag = True
+            target = get_target_price(ticker,0.5)
+            m_open = datetime.datetime(now.year,now.month,now.day) + datetime.timedelta(1.375)
+
+        # BUY if price>target price
+        if get_current_price(ticker) > target and flag:
+            buy_crypto(ticker)
+            print("[BUY] ", str(datetime.datetime.now()))
+            print("PRICE: ",get_current_price(ticker)," TARGET",target)
+            flag = False
+    except:
+        print('[ERR] ', str(datetime.datetime.now()))
+
+    time.sleep(1) #prevents overload
+
